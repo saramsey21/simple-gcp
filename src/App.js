2d2c3b7523016eaf64ed0c9f9a5e35200e302317
saramsey21 <saramsey21@gmail.com>
2023-03-05 15:25:35 -0500

    Add description

--- src/App.js
+++ src/App.js
@@ -4,12 +4,18 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <div className="Header">Welcome To My Chat Bot!</div>
       <iframe
         allow="microphone;"
         width="350"
         height="430"
         src="https://console.dialogflow.com/api-client/demo/embedded/39964328-4541-4d9a-bd25-286beeb183f5">
       </iframe>
+      <div className="Description">
+        <div className="content">To begin using this chatbot, start with a simple greeting (like hello)</div>
+        <div className="content">After answering a few questions, you can ask questions about The University of Cincinnati</div>
+        <div className="content">Try asking 'do they have a football team?' or 'what is in state tuition?' </div>
+      </div>    
     </div>
   );
 }
