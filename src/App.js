dc7906c8f6915ad916230b3eaa3bd9057d5e2a25
saramsey21 <saramsey21@gmail.com>
2023-03-05 02:02:47 -0500

    Add chat box

--- src/App.js
+++ src/App.js
@@ -4,20 +4,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <iframe
+        allow="microphone;"
+        width="350"
+        height="430"
+        src="https://console.dialogflow.com/api-client/demo/embedded/39964328-4541-4d9a-bd25-286beeb183f5">
+      </iframe>
     </div>
   );
 }
